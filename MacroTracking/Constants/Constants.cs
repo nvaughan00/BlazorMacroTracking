b8df268b38274c86821a437d0daaf70396aa7cc1
nvaughan00 <nickvaughan123@gmail.com>
2022-07-20 13:26:02 -0500

    Biggun
    asdasd

---
+++ MacroTracking/Constants/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace MacroTracking.Constants
+{
+    public class Constants
+    {
+        public static string[] ColorGradiant = { "darkred", "orangered", "yellow", "yellowgreen", "forestgreen", "darkgreen" };
+        public static string[] ColorGradiantText = { "white", "darkgrey", "black", "salmon", "palegoldenrod", "palegoldenrod" };
+    }
+}
---
+++ MacroTracking/Models/MacroItem.cs
@@ -0,0 +1,24 @@
+﻿namespace MacroTracking.Models
+{
+    public class MacroItem
+    {
+        public MacroItem(int id, string name, double calories, double protein, double carbs, double fat)
+        {
+            Id = id;
+            Name = name;
+            Calories = calories;
+            Protein = protein;
+            Carbs = carbs;
+            Fat = fat;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+
+        public double Carbs { get; set; }
+
+        public double Fat { get; set; }
+    }
+}
