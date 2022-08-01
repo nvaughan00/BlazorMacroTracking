68d7be039d28d853254f7abff0d43e8c430b36d9
nvaughan00 <nickvaughan123@gmail.com>
2022-08-01 15:23:56 -0500

    biggun

---
+++ MacroTracking/Constants/FoodTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace MacroTracking.Constants
+{
+    public class FoodTypes
+    {
+        public static string BREAKFAST = "Breakfast";
+        public static string SNACK = "Snack";
+        public static string MEAL = "Meal";
+    }
+}
---
+++ MacroTracking/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿namespace MacroTracking.Models
+{
+    public class Item
+    {
+        public Item(int id, string name, decimal calories, decimal protein, decimal carbs, decimal fat, string foodType)
+        {
+            Id = id;
+            Name = name;
+            Calories = calories;
+            Protein = protein;
+            Carbs = carbs;
+            Fat = fat;
+            FoodType = foodType;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Calories { get; set; }
+        public decimal Protein { get; set; }
+        public decimal Carbs { get; set; }
+        public decimal Fat { get; set; }
+        public string FoodType { get; set; }
+    }
+}
