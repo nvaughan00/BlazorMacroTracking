b9fb6384a76fde9b65cc0945fc45a4255ecb46f9
Nvaughan00 <nvaughan@iastate.edu>
2022-08-04 10:16:38 -0500

    fribbles

---
+++ MacroTracking/Services/SqlService.cs
@@ -0,0 +1,16 @@
+﻿using MacroTracking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MacroTracking.Services
+{
+    public partial class SqlService : DbContext
+    {
+        public SqlService() : base()
+        {
+        }
+
+        public virtual DbSet<Item> Items { get; set; }
+
+
+    }
+}
