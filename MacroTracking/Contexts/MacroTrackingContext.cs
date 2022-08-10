cd38338a6b94c442c6bd0435cd495b0bc2fdfc46
nvaughan00 <46831906+nvaughan00@users.noreply.github.com>
2022-08-10 13:50:08 -0500

    Add files via upload

---
+++ MacroTracking/Contexts/MacroTrackingContext.cs
@@ -0,0 +1,39 @@
+﻿using MacroTracking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MacroTracking.Contexts
+{
+    public partial class MacroTrackingContext : DbContext
+    {
+        public virtual DbSet<Item> Items { get ; set; }
+
+        public MacroTrackingContext()
+        {
+            Database.SetCommandTimeout(600);
+        }
+
+        public MacroTrackingContext(DbContextOptions<MacroTrackingContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:MacroTrackingDb");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>(entity =>
+                {
+                    entity.HasKey(x => x.Id);
+
+                    entity.ToTable("ITEM", "dbo");
+
+
+                }
+            );
+        }
+
+    }
+}
